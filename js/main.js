0edbe9bf6c27df78e5a1c7fcee1a78342d905e59
Sebastian <sebas9308@outlook.com>
2021-09-14 13:44:07 -0500

    Se guardan los archivos

---
+++ js/main.js
@@ -0,0 +1,45 @@
+gsap.to('.cuadrado_1',{
+    duration: 3,
+    x: 900,
+    delay: 1,
+    repeat: 20,
+    borderRadius: 50,
+    ease: 'bounce.out',
+    
+    
+
+}); 
+gsap.from('.cuadrado_2',{
+    duration: 3,
+    x: 900,
+    delay: 1,
+    repeat: 20,
+    rotate: 360,
+    y: 100,
+
+    
+    
+
+}); 
+gsap.to('.cuadrado_3',{
+    duration: 3,
+    x: 900,
+    delay: 1,
+    repeat: 20,
+    borderRadius: 50,
+    ease: 'bounce.out',
+    
+
+}); 
+gsap.from('.cuadrado_4',{
+    duration: 3,
+    x: 900,
+    delay: 1,
+    repeat: 20,
+    rotate: 360,
+    y: 100,
+    
+
+    
+
+}); 
